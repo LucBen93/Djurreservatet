d5bf18bd38e557864bb5051bde72bff3e945a25c
LucasB <lucas_benjaminson@hotmail.com>
2022-03-02 18:26:43 +0100

    Fixing gitignore

--- WebApi/Models/Animal.cs
+++ WebApi/Models/Animal.cs
@@ -4,12 +4,19 @@ public class Animal
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public long Time { get; set; }
+    public int MaxHungerLevel { get; set; }
+    public int CurrentHungerLevel { get; set; }
+    public bool EatsMeat { get; set; }
+    public bool EatsLeaf { get; set; }
+
 
     public Animal() { }
-    public Animal(string name, int time)
+    public Animal(string name, int maxHungerLevel, int currentHungerLevel, bool eatsMeat, bool eatsLeaf)
     {
         Name = name;
-        Time = time;
+        MaxHungerLevel = maxHungerLevel;
+        CurrentHungerLevel = currentHungerLevel;
+        EatsMeat = eatsMeat;
+        EatsLeaf = eatsLeaf;
     }
 }
